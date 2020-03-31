77f949242d0f7b6f4505f32ddcf02a0f56edaa0f
fang009 <surya91521@gmail.com>
2020-03-31 14:43:55 +0530

    Project Setup

---
+++ app/src/main/java/com/example/androidflask/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidflask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
