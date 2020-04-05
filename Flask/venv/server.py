28ae6ba5ff0233e529ba365b13c6d04b5dae727e
fang009 <surya91521@gmail.com>
2020-04-05 20:35:56 +0530

    All Clear

--- Flask/venv/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/",methods=['GET','POST'])
-def hello():
-    return "Flask and android connected"
-
-
-app.run(host="0.0.0.0",port=5000,debug=True)   
